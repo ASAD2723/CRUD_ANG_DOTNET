3f0e21abf3c39bd0c5b4411b124a6009864de1c6
ASAD2723 <khanasad323@gmail.com>
2023-03-27 05:22:56 +0100

    Completed

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -21,4 +21,16 @@ export class EmployeesService {
     return this.http.post<Employee>(this.baseApiUrl + 'api/employees',
     addEmployeeRequest);
   }
+
+  getEmployee(id: string): Observable<Employee> {
+    return this.http.get<Employee>(this.baseApiUrl + 'api/employees/' + id)
+  }
+
+  updateEmployee(id: string, updateEmployeeRequest: Employee):Observable<Employee>{
+    return this.http.put<Employee>(this.baseApiUrl + 'api/employees/' + id, updateEmployeeRequest)
+  }
+
+  deleteEmployee(id:string): Observable<Employee>{
+    return this.http.delete<Employee>(this.baseApiUrl + 'api/employees/' + id);
+  }
 }
